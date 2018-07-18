97cea76e35c1d1e1d08e6ea396c0be3ffa848679
ManuelEnrique <enriquealiaga123@gmail.com>
2018-07-18 18:33:37 -0500

    Update FlujoDoc.php

--- app/FlujoDoc.php
+++ app/FlujoDoc.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class FlujoDoc extends Model
 {
-    //
+    protected $table = 'flujo_docs';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'tipo_doc_id',
+        'n_reg',
+        'desc_doc',
+        'fecha',
+        'asunto',
+        'emisor',
+        'archivo',
+        'responsable_id',
+        'se_adjunta',
+        'doc_ref',
+        'area_destino',
+        'estado'
+
+    ];
 }
