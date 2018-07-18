42bfeda4351fbf74044bfa5deb88700330da29d7
ManuelEnrique <enriquealiaga123@gmail.com>
2018-07-18 18:38:42 -0500

    Update Oficina.php

--- app/Oficina.php
+++ app/Oficina.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Oficina extends Model
 {
-    //
+    protected $table = 'oficinas';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'es_area',
+        'es_subarea',
+        'dependencia',
+        'obs'      
+
+    ];
 }
+
