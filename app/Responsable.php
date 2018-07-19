66e0557a45660d386d419d68c9dd1dc0be5a5c9e
ManuelEnrique <enriquealiaga123@gmail.com>
2018-07-18 21:55:08 -0500

    Update Responsable.php

--- app/Responsable.php
+++ app/Responsable.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Responsable extends Model
 {
-    //
+    protected $table = 'responsables';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'user_id',
+        'oficina_id',
+        'f_inicio',
+        'f_fin'    
+
+    ];
 }
