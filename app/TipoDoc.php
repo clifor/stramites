acfb11fb589f264e12b4877804625415d87a80d6
ManuelEnrique <enriquealiaga123@gmail.com>
2018-07-18 21:55:39 -0500

    Update TipoDoc.php

--- app/TipoDoc.php
+++ app/TipoDoc.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoDoc extends Model
 {
-    //
+    protected $table = 'tipo_docs';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'estado'      
+
+    ];
 }
